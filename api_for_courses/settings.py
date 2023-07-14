3050e0504087fa5022f6812cc92b31a90b7fbf14
вNatalya Abrosimova <oxidena86@gmail.com>
2023-07-14 18:15:15 +0700

    debug_false

--- api_for_courses/settings.py
+++ api_for_courses/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lg90gt^3m5=gfso35g&us7bkao+vgd*ksm&_2)een5ar_h1k4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
